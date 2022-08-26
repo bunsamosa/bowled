1f72adbd81e0e797bdf49845484e564cb64f7aa7
madhukar01 <mholla8@gmail.com>
2022-08-27 03:35:18 +0530

    feat(login): social login with arcana

---
+++ src/components/home/LoginComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { AuthProvider } from '@arcana/auth'
+
+// initialize auth provider
+const arcanaAppID = import.meta.env.VITE_ARCANA_APP_ID;
+const auth = new AuthProvider(arcanaAppID);
+await auth.init({
+    //appMode can be 0, 1, or 2 depending upon wallet UI mode that needs to be configured
+    // no ui, widget, full ui modes.
+    appMode: 0,
+    position: "right",
+});
+
+// google login
+// await auth.loginWithSocial("google");
+// await auth.requestSocialLogin('google')
+// const connected = await auth.isLoggedIn();
+
+const provider = auth.provider;
+
+</script>
+<template>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HomeRenderer from "@/components/home/HomeRenderer.vue"
+import LoginComponent from "@/components/home/LoginComponent.vue"
 </script>
 
 <template>
   <v-parallax :scale=1 src="images/background.jpg" class="h-screen">
+    <LoginComponent />
     <HomeRenderer />
   </v-parallax>
 </template>
