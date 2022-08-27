ede80b4f666c84c8e88cd08f0bf70c75a1983ad2
Tejaaswini Narendra <tejnaren07@gmail.com>
2022-08-27 23:21:15 +0530

    update team cards

--- src/components/player-details/PlayerCard.vue
+++ src/components/player-details/PlayerCard.vue
@@ -6,37 +6,43 @@ console.log(jsonData);
 </script>
 
 <template>
+<h1 class="heading">View your team</h1>
+<div class="align-playercards">
+<div v-for="people in jsonData['players']">
     <div class="playerCard">
         <div class="titleBar">
-        <h3>Bob</h3>
-        <h3>23 years 5 days</h3>
+        <h3>{{people.player_name}}</h3>
+        <h3>{{people.age}}</h3>
         </div>
         <div class="player-deets">
-            <h3>Country:</h3>
-            <h3>Experience: </h3>
-            <h3>Form: Moderate</h3>
-            <h3>Fitness: Decent</h3>
-            <!-- <img src="images/batsman.png" /> -->
+            <h3>Country:{{people.country}}</h3>
+            <h3>Experience:{{people.experience}}</h3>
+            <h3>Form: {{people.form}}</h3>
+            <h3>Fitness: {{people.fitness}}</h3>
         </div>
         <div class="skills">Skills
-        <p>fielding: 30%</p>
-        <p>wicket keeping:40%</p>
-        <p>batting vs seam bowlers:35%</p>
-        <p>batting vs spin bowlers:35%</p>
-        <p>bowling main: 45%</p>
-        <p>bowling variation: 50%</p>
+        <p>fielding: {{people.fielding}}</p>
+        <p>wicket keeping:{{people.wicket_keeping}}</p>
+        <p>batting vs seam bowlers:{{people.batting_seam}}</p>
+        <p>batting vs spin bowlers:{{people.batting_spin}}</p>
+        <p>bowling main: {{people["bowling main"]}}</p>
+        <p>bowling variation: {{people.bowling_variation}}</p>
         </div>
     </div>
-
+</div>
+</div>
 </template>
 
 <style>
     .playerCard{
         background-color: rgb(107, 164, 211);
         width: 20vw !important;
-        height: 80vh;
+        height: 58vh;
         border-radius: 30px;
         color:white;
+        float: left;
+        margin-right: 20px !important;
+        margin-bottom: 20px !important;
     }
     .titleBar{
         background-color: rgb(64, 146, 213);
@@ -55,6 +61,18 @@ console.log(jsonData);
     .skills{
         margin-left: 20px;
         margin-top: 20px;
+    }
+    .json-data{
+        display: flex;
+        flex-direction: row !important;
+    }
+    .align-playercards{
+        margin-left:8vw !important;
+        margin: 5% 0;
+    }
+    .heading{
+        text-align: center;
+        margin-top: 5%;
     }
 </style>
 
