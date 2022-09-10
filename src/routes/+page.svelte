72a41f92a02af14bce9793ca2a5bd9b9942a782a
madhukar01 <mholla8@gmail.com>
2022-09-10 22:50:50 +0530

    feat(home): landing page teaser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-3xl font-bold">Welcome to Bowled</h1>
+<div class="hero min-h-screen bg-black">
+	<div class="hero-content text-center">
+		<div class="max-w-2xl">
+			<img src="images/hero.png" class="my-5" />
+			<p class="text-white text-xl">Coming Soon</p>
+		</div>
+	</div>
+</div>
