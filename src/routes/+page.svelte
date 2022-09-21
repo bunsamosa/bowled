d64f59bb5f36b78cc7314bce54cafc60057fa5c9
madhukar01 <mholla8@gmail.com>
2022-09-22 04:14:35 +0530

    feat(home): add nav bar and wiki

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,26 @@
-<div class="hero min-h-screen bg-black">
-	<div class="hero-content text-center">
-		<div class="max-w-2xl">
-			<img src="images/hero.png" class="my-5" />
-			<p class="text-white text-xl">Coming Soon</p>
+<div class="bg-black">
+	<div class="flex flex-col min-h-screen max-w-screen-xl m-auto">
+		<div class="navbar flex-none">
+			<div class="flex-1">
+				<a class="btn btn-ghost normal-case text-2xl" href="/">Bowled</a>
+			</div>
+			<div class="flex-none">
+				<ul class="menu menu-horizontal p-0">
+					<li><a href="https://wiki.bunsamosa.org/" class="btn btn-ghost normal-case">Wiki</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="hero flex-1">
+			<div class="hero-content text-center">
+				<div class="max-w-2xl">
+					<img
+						src="images/hero.png"
+						class="my-5"
+						alt="You are the owner of a T20 Cricket franchise"
+					/>
+					<p class="text-white text-xl">Coming Soon</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
