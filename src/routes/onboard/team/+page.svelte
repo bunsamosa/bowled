73de9087aa72b94e7f9f00d9bc5045ee612a4d89
madhukar01 <mholla8@gmail.com>
2022-09-26 02:01:06 +0530

    feat(onboarding): user and team onboarding

--- src/routes/onboard/team/+page.svelte
+++ src/routes/onboard/team/+page.svelte
@@ -5,12 +5,12 @@
 
 	// form validator
 	const form = useForm({
-		managerName: { validators: [required, minLength(3)] }
+		teamName: { validators: [required, minLength(3)] }
 	});
 
 	// move to team creation page
-	function createTeam() {
-		goto('/onboard/team');
+	function loadHome() {
+		goto('/game/home');
 	}
 </script>
 
@@ -20,37 +20,36 @@
 			autoplay
 			loop
 			mode="normal"
-			src="https://assets7.lottiefiles.com/packages/lf20_M9p23l.json"
+			src="https://assets9.lottiefiles.com/packages/lf20_n0m2eaa8.json"
 			class="max-w-md rounded-lg shadow-2xl"
 		/>
 		<div>
-			<h1 class="text-5xl font-bold py-3">Welcome to Bowled!</h1>
+			<h1 class="text-3xl font-bold py-3">Let's give our team a catchy name</h1>
 			<p class="py-5 text-white">
-				Hello, my name is XYZ, and I'm your assistant.
+				Hello XYZ,
 				<br />
-				I will assist you in managing our franchise.
+				Our players are excited to meet you ...
 			</p>
 			<div class="form-control w-full max-w-xs py-3">
 				<form use:form>
-					<label class="label" for="managerName">
-						<span class="label-text">How should I address you?</span>
+					<label class="label" for="teamName">
+						<span class="label-text">What should we call our franchise?</span>
 					</label>
 					<!-- svelte-ignore a11y-autofocus -->
 					<input
 						type="text"
-						placeholder="Type your name here"
+						placeholder="Type team name here"
 						class="input input-bordered w-full max-w-xs text-white"
-						id="managerName"
-						name="managerName"
+						id="teamName"
+						name="teamName"
 					/>
-					<Hint class="label-text text-red-500" for="managerName" on="minLength" let:value>
-						Name should be atleast {value} characters long
+					<Hint class="label-text text-red-500" for="teamName" on="minLength" let:value>
+						Team name should be atleast {value} characters long
 					</Hint>
 				</form>
 			</div>
 			<div class="py-5">
-				<button class="btn btn-outline" disabled={!$form.valid} on:click={createTeam}
-					>Continue</button
+				<button class="btn btn-outline" disabled={!$form.valid} on:click={loadHome}>Continue</button
 				>
 			</div>
 		</div>
