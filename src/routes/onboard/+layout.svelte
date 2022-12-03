267469279814a51e2bfa8fcb480fbf0eef64d655
madhukar01 <mholla8@gmail.com>
2022-12-03 11:41:56 +0530

    feat(login): user auth

--- src/routes/onboard/+layout.svelte
+++ src/routes/onboard/+layout.svelte
@@ -1,3 +1,36 @@
+<script lang="ts">
+	import { authClient } from '$lib/utils/authClient';
+	import { appSession } from '$lib/stores/sessionStore';
+	import { goto } from '$app/navigation';
+
+	// set app session from auth
+	// appSession.set(authClient.auth.session());
+
+	// update app session on auth state change
+	authClient.auth.onAuthStateChange((event, session) => {
+		appSession.set(session);
+
+		// if user is not logged in - send him to home page
+		if (!$appSession) {
+			console.log('User not logged in');
+			goHome();
+		} else {
+			// check user status
+		}
+	});
+
+	// signout and go home
+	async function signout() {
+		const { error } = await authClient.auth.signOut();
+		goHome();
+	}
+
+	// navigate to home page
+	function goHome() {
+		goto('/');
+	}
+</script>
+
 <!-- Layout for onboarding pages -->
 <div class="h-screen max-w-screen-xl m-auto flex flex-col">
 	<div class="navbar flex-none">
@@ -5,7 +38,7 @@
 			<a class="btn btn-ghost normal-case text-2xl" href="/">Bowled</a>
 		</div>
 		<div class="flex-none">
-			<button class="btn btn-ghost normal-case">Exit</button>
+			<button class="btn btn-ghost normal-case" on:click={signout}>Exit</button>
 		</div>
 	</div>
 	<slot />
