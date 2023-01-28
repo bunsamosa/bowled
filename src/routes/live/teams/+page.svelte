7317a6bf990597f0ea89e028bf179b660f1b01ae
madhukar01 <mholla8@gmail.com>
2023-01-28 11:38:10 -0500

    live(teams): choose teams page

---
+++ src/routes/live/teams/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { serverURL } from '$lib/utils/bowledClient';
+	import { appSession } from '$lib/stores/sessionStore';
+
+	let teamData: any = [];
+
+	async function loadTeams() {
+		// load teams data
+		// let access_token = $appSession?.access_token;
+		let url = serverURL + '/live/teams';
+		console.log(url);
+		const response = await fetch(url);
+		if (response.status == 200) {
+			let teams = await response.json();
+			teamData = [...teams];
+		}
+	}
+</script>
+
+<div class="hero flex-1">
+	<div class="hero-content flex-col lg:flex-row flex-wrap">
+		{#await loadTeams() then}
+			{#each teamData as team}
+				<div class="card w-96 bg-base-100 shadow-xl">
+					<figure>
+						<img src={team.team_logo} alt={team.team_name} class="max-h-40" />
+					</figure>
+					<div class="card-body">
+						<h2 class="card-title">{team.team_name}</h2>
+						<div class="card-actions justify-end">
+							<button class="btn btn-primary">Pick</button>
+						</div>
+					</div>
+				</div>
+			{/each}
+		{/await}
+	</div>
+</div>
