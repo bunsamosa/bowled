32f8c9be54d4d15e877d7c5f189c5524c5cff6ea
madhukar01 <mholla8@gmail.com>
2023-02-02 11:38:17 -0500

    fix(teams): update choose team layout

--- src/routes/live/+layout.svelte
+++ src/routes/live/+layout.svelte
@@ -3,23 +3,25 @@
 	import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 </script>
 
-<!-- Layout for onboarding pages -->
-<div class="sticky z-50 top-0 navbar max-w-screen-xl m-auto flex-none">
-	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-2xl" href="/">Bowled</a>
+<div class="max-w-screen-xl m-auto">
+	<!-- Nav bar -->
+	<div class="sticky z-50 top-0 navbar max-w-screen-xl">
+		<div class="flex-1">
+			<a class="btn btn-ghost normal-case text-2xl" href="/">Bowled</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal p-0">
+				<li>
+					<!-- Twitter icon -->
+					<a href="https://twitter.com/bowledGG" target="_blank"><Fa icon={faTwitter} /></a>
+				</li>
+				<li>
+					<a href="https://wiki.bunsamosa.org/" target="_blank" class="btn btn-ghost normal-case"
+						>Wiki</a
+					>
+				</li>
+			</ul>
+		</div>
 	</div>
-	<div class="flex-none">
-		<ul class="menu menu-horizontal p-0">
-			<li>
-				<!-- Twitter icon -->
-				<a href="https://twitter.com/bowledGG" target="_blank"><Fa icon={faTwitter} /></a>
-			</li>
-			<li>
-				<a href="https://wiki.bunsamosa.org/" target="_blank" class="btn btn-ghost normal-case"
-					>Wiki</a
-				>
-			</li>
-		</ul>
-	</div>
+	<slot />
 </div>
-<slot />
--- src/routes/live/teams/+page.svelte
+++ src/routes/live/teams/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { serverURL } from '$lib/utils/bowledClient';
-	import { appSession } from '$lib/stores/sessionStore';
 	import { goto } from '$app/navigation';
 
 	let teamData: any = [];
@@ -23,23 +22,30 @@
 </script>
 
 <div class="hero flex-1">
-	<div class="hero-content flex-col lg:flex-row flex-wrap">
-		{#await loadTeams() then}
-			{#each teamData as team}
-				<div class="card w-96 bg-base-100 shadow-xl">
-					<figure>
-						<img src={team.team_logo} alt={team.team_name} class="max-h-40" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">{team.team_name}</h2>
-						<div class="card-actions justify-end">
-							<button class="btn btn-primary" on:click={() => chooseOpponent(team.team_id)}
-								>Pick</button
-							>
+	<div class="hero-content flex-col min-w-full">
+		<div class="my-5"><h1 class="text-5xl font-bold">Choose your favorite team</h1></div>
+		<div class="min-w-full">
+			<div class="flex flex-row flex-wrap">
+				{#await loadTeams() then}
+					{#each teamData as team}
+						<div
+							class="card w-72 h-72 mx-auto mb-10 shadow-xl image-full hover:scale-110 hover:bg-blue-600"
+						>
+							<figure>
+								<img src={team.team_logo} alt={team.team_name} />
+							</figure>
+							<div class="card-body justify-around">
+								<h2 class="card-title justify-center text-3xl">{team.team_name}</h2>
+								<div class="card-actions justify-end">
+									<button class="btn btn-primary" on:click={() => chooseOpponent(team.team_id)}
+										>Pick</button
+									>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			{/each}
-		{/await}
+					{/each}
+				{/await}
+			</div>
+		</div>
 	</div>
 </div>
