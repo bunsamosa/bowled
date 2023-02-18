551a610975176f6dae8b898357b940d9ae783702
madhukar01 <mholla8@gmail.com>
2023-02-18 16:57:11 -0500

    fix(navbar): make navbar sticky on internal pages

--- src/lib/components/core/HomeNavbar.svelte
+++ src/lib/components/core/HomeNavbar.svelte
@@ -8,7 +8,7 @@
 	import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 	import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
 
-	$: themeChangeAllowed = $page.url.pathname != '/';
+	$: notHome = $page.url.pathname != '/';
 	// NOTE: the element that is using one of the theme attributes must be in the DOM on mount
 	onMount(() => {
 		themeChange(false);
@@ -17,22 +17,13 @@
 </script>
 
 <!-- Navbar -->
-<div class="fixed z-50 top-2 navbar max-w-screen-xl backdrop-blur-xl">
+<div class="{notHome ? 'sticky' : 'fixed'} z-50 top-2 navbar max-w-screen-xl backdrop-blur-xl">
 	<div class="flex-1">
 		<a class="btn btn-ghost normal-case text-2xl" href="/">Bowled</a>
 	</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal p-0 ">
-			<li>
-				<!-- Twitter icon -->
-				<a href="https://twitter.com/bowledGG" target="_blank"><Fa icon={faTwitter} /></a>
-			</li>
-			<!-- <li>
-				<a href="https://wiki.bunsamosa.org/" target="_blank" class="btn btn-ghost normal-case"
-					>Wiki</a
-				>
-			</li> -->
-			{#if themeChangeAllowed}
+			{#if notHome}
 				<li>
 					<button
 						data-toggle-theme="halloween,bumblebee"
@@ -43,6 +34,15 @@
 					>
 				</li>
 			{/if}
+			<li>
+				<!-- Twitter icon -->
+				<a href="https://twitter.com/bowledGG" target="_blank"><Fa icon={faTwitter} /></a>
+			</li>
+			<!-- <li>
+				<a href="https://wiki.bunsamosa.org/" target="_blank" class="btn btn-ghost normal-case"
+					>Wiki</a
+				>
+			</li> -->
 		</ul>
 	</div>
 </div>
