85d0dccd60c328cd69cc593f8032830c560823bf
madhukar01 <mholla8@gmail.com>
2023-02-18 22:24:53 -0500

    fix(game): end of game and streaming bugfixes

--- src/routes/live/match/+page.svelte
+++ src/routes/live/match/+page.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
 	import { serverURL } from '$lib/utils/bowledClient';
 	import { goto } from '$app/navigation';
-	import savedGame from './gameData.json';
 	import { teamID, battingLineUp, bowlingLineUp } from '$lib/stores/gameStore';
+	import BallLoader from '$lib/components/core/BallLoader.svelte';
 
-	let myTeam: string;
-	let gameData = savedGame;
+	let myTeam: string = $teamID;
+	let gameData: any = {};
 	let batting: any = [];
 	let bowling: any = [];
-	let payload;
-	myTeam = $teamID;
+	let payload: any = {};
+	let current_over_data = [];
+	let full_match_commentary = [];
+	let current_ball = 0;
+	let current_ball_data = {};
+	let current_over = 0;
+	let previous_over = 0;
+	let allPlayers = {};
+	let batsman = {};
+	let bowler = {};
+	let streaming = false;
+	let timeoutID: any;
+	let runs_required = 0;
 
-	async function loadGame() {
+	onMount(async () => {
+		// load lineup data from store
 		try {
 			batting = JSON.parse($battingLineUp);
 			bowling = JSON.parse($bowlingLineUp);
@@ -38,7 +51,7 @@
 			goto('/live/teams');
 		}
 
-		// load player data
+		// send lineup data to server and fetch game data
 		let url = serverURL + '/live/game';
 		const response = await fetch(url, {
 			method: 'POST',
@@ -52,35 +65,26 @@
 			let data = await response.json();
 			gameData = data;
 			current_ball_data = gameData.match_commentary[current_ball];
+			full_match_commentary = gameData.match_commentary;
+			allPlayers = {
+				...gameData.first_innings.batting_team,
+				...gameData.second_innings.batting_team
+			};
+			streamData();
+		} else {
+			alert("We're sorry, something went wrong. Please try again later.");
+			goto('/');
 		}
-		streamData();
-	}
+	});
 
-	function isDisabled(a, b) {
-		console.log(a, b, a >= b);
-		return a >= b;
-	}
+	function streamData() {
+		console.log('Streaming: Ball ' + current_ball + ' of ' + full_match_commentary.length);
 
-	let current_over_data = [];
-	// stream match data from server
-	$: full_match_commentary = gameData.match_commentary;
-	// $: current_ball = Math.floor(full_match_commentary.length / 2);
-	$: current_ball = 0;
-	$: current_ball_data = full_match_commentary[current_ball];
-	$: player1 = current_ball_data.striker;
-	$: player2 = current_ball_data.bowler;
+		if (current_ball < full_match_commentary.length) {
+			current_ball_data = full_match_commentary[current_ball];
 
-	if (current_ball_data?.innings == 'Second') {
-		player1 = current_ball_data.striker;
-		player2 = current_ball_data.bowler;
-	}
-	async function streamData() {
-		let current_over = 0;
-		let previous_over = 0;
-
-		current_over_data = [];
-		while (current_ball < full_match_commentary.length) {
-			previous_over = current_ball_data.current_over;
+			batsman = allPlayers[current_ball_data.striker?.player_id];
+			bowler = allPlayers[current_ball_data.bowler?.player_id];
 			current_ball_data = full_match_commentary[current_ball];
 			current_over = current_ball_data.current_over;
 			current_over_data = [current_ball_data, ...current_over_data];
@@ -88,19 +92,30 @@
 			if (current_over != previous_over) {
 				current_over_data = [current_ball_data];
 			}
-			await sleep(3000);
-			current_ball += 1;
+
+			current_ball++;
+			previous_over = current_ball_data.current_over;
+			if (current_ball < full_match_commentary.length) {
+				streaming = true;
+				timeoutID = setTimeout(streamData, 3000);
+			}
+
+			if (current_ball_data['innings'] == 'Second') {
+				runs_required = current_ball_data['target'] - current_ball_data['current_score'];
+			}
 		}
 	}
 
-	function sleep(ms) {
-		return new Promise((resolve) => setTimeout(resolve, ms ?? 200));
-	}
+	onDestroy(() => {
+		if (streaming) {
+			clearTimeout(timeoutID);
+		}
+	});
 </script>
 
-<div class="h-screen max-w-screen-xl m-auto flex flex-col">
+<div class="min-h-screen max-w-screen-xl m-auto flex flex-col mt-5">
 	<div>
-		{#await loadGame() then}
+		{#if streaming}
 			<div class="flex w-full flex-col lg:flex-row">
 				{#if current_ball_data.innings == 'First'}
 					<div class="stat text-left bg-primary text-primary-content rounded-box">
@@ -127,7 +142,7 @@
 					</div>
 				{:else}
 					<div class="stat text-left bg-base-200 rounded-box">
-						<div class="stat-title">Won the toss, chose to bat first</div>
+						<div class="stat-title">{gameData.toss_result}</div>
 						<div class="stat-value text-black">{gameData.team_name}</div>
 						<div class="stat-value ">
 							<span class="text-3xl"
@@ -141,7 +156,17 @@
 					<div class="divider divider-horizontal">vs</div>
 					<div class="stat text-right bg-primary text-primary-content rounded-box">
 						<div class="stat-title">
-							Chasing {current_ball_data.target - current_ball_data.current_score}
+							{#if runs_required > 0}
+								{#if current_ball == full_match_commentary.length}
+									Lost the game by {runs_required - 1} runs
+								{:else}
+									Need {runs_required} runs to win
+								{/if}
+							{:else if runs_required == 0}
+								Match tied
+							{:else}
+								Won the game with {10 - current_ball_data.current_wickets} wickets left
+							{/if}
 						</div>
 						<div class="stat-value text-black">{gameData.enemy_team}</div>
 						<div class="stat-value ">
@@ -157,28 +182,18 @@
 				{/if}
 			</div>
 			<div class="p-5 divider-horizontal" />
-			{#each current_over_data as ball_data}
-				<div class="alert alert-success shadow-2xl font-bold mb-3">
-					<div>
-						{ball_data.current_over}.{ball_data.current_ball}
-						{ball_data.bowler?.player_name} to {ball_data.striker?.player_name}
-						<span>{ball_data.outcome_label}</span>
-					</div>
-				</div>
-			{/each}
-			<div class="p-5 divider-horizontal" />
 			<div class="flex flex-col w-full lg:flex-row">
 				<div class="card bg-base-100 shadow-xl flex-1">
 					<div class="card-body">
 						<h2 class="card-title text-xl font-bold">
-							{player1.player_name}
+							{batsman.player_name}
 						</h2>
 						<div class="card-actions justify-start">
-							{#if player1.player_type == 'batsman'}
-								<div class="badge badge-outline">{player1.batting_type}</div>
-							{:else if player1.player_type == 'bowler'}
-								<div class="badge badge-outline">{player1.bowling_type}</div>
-							{:else if player1.player_type == 'all-rounder'}
+							{#if batsman.player_type == 'batsman'}
+								<div class="badge badge-outline">{batsman.batting_type}</div>
+							{:else if batsman.player_type == 'bowler'}
+								<div class="badge badge-outline">{batsman.bowling_type}</div>
+							{:else if batsman.player_type == 'all-rounder'}
 								<div class="badge badge-outline">All Rounder</div>
 							{:else}
 								<div class="badge badge-outline">Wicket Keeper</div>
@@ -188,18 +203,18 @@
 						<div class="flex flex-row">
 							<p class="text-left">
 								<span>Form</span>
-								<span class="badge">{player1.form}</span>
+								<span class="badge">{batsman.form}</span>
 							</p>
 							<p class="text-right">
 								<span>Fitness</span>
-								<span class="badge">{player1.fitness}</span>
+								<span class="badge">{batsman.fitness}</span>
 							</p>
 						</div>
 						<div class="divider-horizontal" />
 						<div>
 							<div class="rating rating-lg">
 								{#each Array(10) as _, i}
-									{#if i + 1 == player1.batting_rating}
+									{#if i + 1 == batsman.batting_rating}
 										<input
 											type="radio"
 											name="rating-batsman"
@@ -211,6 +226,7 @@
 											type="radio"
 											name="rating-batsman"
 											class="mask mask-star-2 bg-orange-400"
+											disabled
 										/>
 									{/if}
 								{/each}
@@ -222,14 +238,14 @@
 				<div class=" card bg-base-100 shadow-xl flex-1">
 					<div class="card-body">
 						<h2 class="card-title">
-							{player2.player_name}
+							{bowler.player_name}
 						</h2>
 						<div class="card-actions justify-start">
-							{#if player2.player_type == 'batsman'}
-								<div class="badge badge-outline">{player2.batting_type}</div>
-							{:else if player2.player_type == 'bowler'}
-								<div class="badge badge-outline">{player2.bowling_type}</div>
-							{:else if player2.player_type == 'all-rounder'}
+							{#if bowler.player_type == 'batsman'}
+								<div class="badge badge-outline">{bowler.batting_type}</div>
+							{:else if bowler.player_type == 'bowler'}
+								<div class="badge badge-outline">{bowler.bowling_type}</div>
+							{:else if bowler.player_type == 'all-rounder'}
 								<div class="badge badge-outline">All Rounder</div>
 							{:else}
 								<div class="badge badge-outline">Wicket Keeper</div>
@@ -239,17 +255,17 @@
 						<div class="flex flex-row">
 							<p class="text-left">
 								<span>Form</span>
-								<span class="badge">{player2.form}</span>
+								<span class="badge">{bowler.form}</span>
 							</p>
 							<p class="text-right">
 								<span>Fitness</span>
-								<span class="badge">{player2.fitness}</span>
+								<span class="badge">{bowler.fitness}</span>
 							</p>
 						</div>
 						<div class="divider-horizontal" />
 						<div class="rating rating-lg">
 							{#each Array(10) as _, i}
-								{#if i + 1 == player2.bowling_rating}
+								{#if i + 1 == bowler.bowling_rating}
 									<input
 										type="radio"
 										name="rating-bowler"
@@ -257,13 +273,29 @@
 										checked
 									/>
 								{:else}
-									<input type="radio" name="rating-bowler" class="mask mask-star-2 bg-orange-400" />
+									<input
+										type="radio"
+										name="rating-bowler"
+										class="mask mask-star-2 bg-orange-400"
+										disabled
+									/>
 								{/if}
 							{/each}
 						</div>
 					</div>
 				</div>
 			</div>
-		{/await}
+			<div class="p-5 divider-horizontal" />
+			{#each current_over_data as ball_data}
+				<div class="alert alert-success shadow-2xl font-bold mb-3">
+					<div>
+						{ball_data.current_over}.{ball_data.current_ball}
+						{ball_data.bowler?.player_name} to {ball_data.striker?.player_name}
+						<span>{ball_data.outcome_label}</span>
+					</div>
+				</div>
+			{/each}
+		{:else}<BallLoader />
+		{/if}
 	</div>
 </div>
