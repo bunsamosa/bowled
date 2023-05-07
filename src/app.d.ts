2f8ad9355b101df9494c18ee417b87757a819502
madhukar01 <mholla8@gmail.com>
2023-05-07 14:13:26 -0400

    fix(dependencies): skeleton theme and font

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,3 @@ declare namespace App {
 	// interface PageData {}
 	// interface Platform {}
 }
-
-declare module '@fortawesome/pro-solid-svg-icons/index.es' {
-	export * from '@fortawesome/pro-solid-svg-icons';
-}
