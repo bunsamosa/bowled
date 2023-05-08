e5e873baee3cd02fbf06910e6d7ff702ea15a721
madhukar01 <mholla8@gmail.com>
2023-05-08 19:52:22 -0400

    fix(home): add click action to feature badge

--- src/lib/components/landingPage/NewFeature.svelte
+++ src/lib/components/landingPage/NewFeature.svelte
@@ -3,12 +3,16 @@
 	import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 	export let featureText = 'New Feature';
+	export let onClickAction = () => {};
 </script>
 
-<div class="h-10 m-auto max-w-[240px] py-1 px-1 rounded-full bg-gray-800 hover:bg-gray-700">
+<button
+	class="h-10 m-auto max-w-[240px] py-1 px-1 rounded-full bg-gray-800 hover:bg-gray-700"
+	on:click={onClickAction}
+>
 	<span class="flex flex-row items-center">
 		<div class="text-xs bg-secondary-600 rounded-full px-4 py-1.5">New</div>
 		<div class="text-sm font-medium px-2 py-1.5">{featureText}</div>
 		<Fa size="lg" icon={faAngleRight} />
 	</span>
-</div>
+</button>
--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 	import Hero from '$lib/components/landingPage/Hero.svelte';
 	import NewFeature from '$lib/components/landingPage/NewFeature.svelte';
+	import { goto } from '$app/navigation';
 
 	export let data;
+
+	// navigate to game
+	async function play() {
+		goto('/live/teams');
+	}
 </script>
 
 <div class="h-screen flex flex-col w-full text-center py-8 px-4 lg:py-16 lg:px-12">
 	<div class="m-auto items-center">
 		<!-- New feature badge -->
-		<NewFeature featureText="IPL season pack is out!" />
+		<NewFeature featureText="IPL season pack is out!" onClickAction={play} />
 		<!-- Copy -->
 		<Hero metrics={data.metrics} />
 	</div>
