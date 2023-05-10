67c0c53ca707d56c36950fc0b023d4aa35b3d528
madhukar01 <mholla8@gmail.com>
2023-05-10 16:52:25 -0400

    feat(teams): teams list component

---
+++ src/lib/components/teams/LiveTeamList.svelte
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+	import LiveTeam from '$lib/components/teams/LiveTeam.svelte';
+	export let data: any = [];
+</script>
+
+<div class="flex flex-row flex-wrap justify-center items-center">
+	{#each data as team}
+		<LiveTeam data={team} />
+	{/each}
+</div>
