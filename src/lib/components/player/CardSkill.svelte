bde7ff960d8c7d6d5a99d803a36f6c9b335a186b
madhukar01 <mholla8@gmail.com>
2023-05-17 11:44:20 -0400

    feat(players): player skill component

---
+++ src/lib/components/player/CardSkill.svelte
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+	import { ProgressBar } from '@skeletonlabs/skeleton';
+	export let skillName: string = '';
+	export let skillColor: string = '';
+	export let skillLevel: string = '';
+	export let skillIndex: number = 0;
+	export let skillMin: number = 0;
+	export let skillMax: number = 20;
+</script>
+
+<div class="flex flex-col w-full m-1">
+	<div class="flex flex-row justify-between w-full">
+		<span class="font-semibold">{skillName}</span>
+		<span class="font-semibold" style="color:{skillColor}">
+			{skillLevel}
+		</span>
+	</div>
+	<span class="my-1" style="">
+		<ProgressBar
+			min={skillMin}
+			max={skillMax}
+			label={skillName}
+			value={skillIndex}
+			meter="bg-yellow-500"
+			track="bg-gray-300"
+		/>
+	</span>
+</div>
